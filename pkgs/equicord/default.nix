6c8bff2f97697c84bb11f1f2f237bad622abc126
4evy <git@evy.pink>
2026-09-10 01:30:00 +0300

    pkgs: pass pinned pnpm through upstream package overrides

--- pkgs/equicord/default.nix
+++ pkgs/equicord/default.nix
@@ -7,6 +7,7 @@
   nix-update-script,
 }:
 let
+  pnpm = callPackage ../pnpm { };
   version = "1.15.4.0-2026-09-05";
   rev = "3b617d4f02be78808dfbfc1b192e1fd20fe5db00";
   hash = "sha256-ueea3PGBVLjnpMxzQ1dmEuLq9Hj6zF3o+ODT7eD+1To=";
@@ -29,42 +30,42 @@ let
     ];
   };
 in
-(equicord.override { inherit buildWebExtension; }).overrideAttrs (
-  oldAttrs:
-  let
-    pnpm = callPackage ../pnpm { };
-    patches = (oldAttrs.patches or [ ]) ++ [
-      ./equicord-content-warning-settings.patch
-    ];
-  in
-  {
-    inherit version src patches;
-    pnpmDeps = fetchPnpmDeps {
-      inherit
-        src
-        version
-        patches
-        pnpm
-        ;
-      inherit (oldAttrs) pname;
-      prePnpmInstall = ''
-        export NODE_OPTIONS=--max-old-space-size=2048
-        export pnpm_config_child_concurrency=1
-        export pnpm_config_network_concurrency=1
-        export pnpm_config_workspace_concurrency=1
-      '';
-      fetcherVersion = 4;
-      hash = pnpmDepsHash;
-    };
-    nativeBuildInputs =
-      builtins.filter (input: (input.pname or "") != "pnpm") (oldAttrs.nativeBuildInputs or [ ])
-      ++ [ pnpm ];
-    passthru = (oldAttrs.passthru or { }) // {
-      inherit updateScript;
-    };
-    env = (oldAttrs.env or { }) // {
-      EQUICORD_REMOTE = "${owner}/${repo}";
-      EQUICORD_HASH = "${rev}";
-    };
-  }
-)
+(equicord.override {
+  inherit buildWebExtension;
+  pnpm_10 = pnpm;
+}).overrideAttrs
+  (
+    oldAttrs:
+    let
+      patches = (oldAttrs.patches or [ ]) ++ [
+        ./equicord-content-warning-settings.patch
+      ];
+    in
+    {
+      inherit version src patches;
+      pnpmDeps = fetchPnpmDeps {
+        inherit
+          src
+          version
+          patches
+          pnpm
+          ;
+        inherit (oldAttrs) pname;
+        prePnpmInstall = ''
+          export NODE_OPTIONS=--max-old-space-size=2048
+          export pnpm_config_child_concurrency=1
+          export pnpm_config_network_concurrency=1
+          export pnpm_config_workspace_concurrency=1
+        '';
+        fetcherVersion = 4;
+        hash = pnpmDepsHash;
+      };
+      passthru = (oldAttrs.passthru or { }) // {
+        inherit updateScript;
+      };
+      env = (oldAttrs.env or { }) // {
+        EQUICORD_REMOTE = "${owner}/${repo}";
+        EQUICORD_HASH = "${rev}";
+      };
+    }
+  )
--- pkgs/vencord/default.nix
+++ pkgs/vencord/default.nix
@@ -7,6 +7,7 @@
   nix-update-script,
 }:
 let
+  pnpm = callPackage ../pnpm { };
   version = "1.15.4-2026-08-30";
   rev = "0e40e433d7aa9168f656aba733d01e761b7ca8ca";
   hash = "sha256-IoyxQuFrTlpwTqYgqsbeoLMuw8Hh7IJlvQi7ULdNAR0=";
@@ -16,60 +17,60 @@ let
     inherit rev hash;
   };
 in
-(vencord.override { inherit buildWebExtension; }).overrideAttrs (
-  oldAttrs:
-  let
-    pnpm = callPackage ../pnpm { };
-    patches = [ ];
-    postPatch = "";
-  in
-  {
-    inherit
-      version
-      src
-      patches
-      postPatch
-      ;
-    pnpmDeps = fetchPnpmDeps {
-      inherit (oldAttrs) pname;
+(vencord.override {
+  inherit buildWebExtension;
+  pnpm_11 = pnpm;
+}).overrideAttrs
+  (
+    oldAttrs:
+    let
+      patches = [ ];
+      postPatch = "";
+    in
+    {
       inherit
-        pnpm
+        version
+        src
         patches
         postPatch
-        src
         ;
-      prePnpmInstall = ''
-        export NODE_OPTIONS=--max-old-space-size=2048
-        export pnpm_config_child_concurrency=1
-        export pnpm_config_network_concurrency=1
-        export pnpm_config_workspace_concurrency=1
-      '';
-      fetcherVersion = 4;
-      hash = pnpmDepsHash;
-    };
-    env = (oldAttrs.env or { }) // {
-      VENCORD_REMOTE = "${src.owner}/${src.repo}";
-      VENCORD_HASH = rev;
-    };
-    meta = oldAttrs.meta // {
-      description =
-        if buildWebExtension then
-          "Vencord web extension"
-        else
-          oldAttrs.meta.description or "Vencord Discord client mod";
-    };
-    passthru = (oldAttrs.passthru or { }) // {
-      updateScript = nix-update-script {
-        attrPath = "vencord";
-        extraArgs = [
-          "--flake"
-          "--version=branch=main"
-          "--override-filename=pkgs/vencord/default.nix"
-        ];
+      pnpmDeps = fetchPnpmDeps {
+        inherit (oldAttrs) pname;
+        inherit
+          pnpm
+          patches
+          postPatch
+          src
+          ;
+        prePnpmInstall = ''
+          export NODE_OPTIONS=--max-old-space-size=2048
+          export pnpm_config_child_concurrency=1
+          export pnpm_config_network_concurrency=1
+          export pnpm_config_workspace_concurrency=1
+        '';
+        fetcherVersion = 4;
+        hash = pnpmDepsHash;
       };
-    };
-    nativeBuildInputs =
-      builtins.filter (input: (input.pname or "") != "pnpm") (oldAttrs.nativeBuildInputs or [ ])
-      ++ [ pnpm ];
-  }
-)
+      env = (oldAttrs.env or { }) // {
+        VENCORD_REMOTE = "${src.owner}/${src.repo}";
+        VENCORD_HASH = rev;
+      };
+      meta = oldAttrs.meta // {
+        description =
+          if buildWebExtension then
+            "Vencord web extension"
+          else
+            oldAttrs.meta.description or "Vencord Discord client mod";
+      };
+      passthru = (oldAttrs.passthru or { }) // {
+        updateScript = nix-update-script {
+          attrPath = "vencord";
+          extraArgs = [
+            "--flake"
+            "--version=branch=main"
+            "--override-filename=pkgs/vencord/default.nix"
+          ];
+        };
+      };
+    }
+  )
